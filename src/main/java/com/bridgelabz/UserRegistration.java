aec8827c96ae0ea785f7b6d274aaf9cca722ec58
sainathphad <phadsainath1@gmail.com>
2021-05-25 23:39:01 +0530

    ADDED:Pattern to validate first name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
 
-public class UserRegistration {
-    public static void main(String[] args) {
 
+public class UserRegistration {
+
+    String namePattern = "^[A-Z][a-z]{3,}$";
+
+    //PATTERN CHECK
+    public boolean checkPattern(String firstName){
+        boolean result = firstName.matches(namePattern);
+        return result;
     }
+
 }
